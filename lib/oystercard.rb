f35430beffd670661e66e67186d1ef88909e7bff
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-07-10 13:22:33 +0100

    Add top_up method to Oystercard

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,4 +6,8 @@ class Oystercard
     @balance = balance
   end
 
+  def top_up(amount)
+    @balance += amount
+    amount
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,5 +4,11 @@ describe Oystercard do
   subject(:oystercard) {described_class.new}
 
   it { is_expected.to respond_to :balance }
-  
+
+  describe '#top_up' do
+    it 'allows top-ups' do
+      @test_amount = rand(100)
+      expect(oystercard.top_up(@test_amount)).to eq @test_amount
+    end
+  end
 end
